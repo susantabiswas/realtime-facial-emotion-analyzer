28448fe2f8d1bfe4987c1d07a152cb0f2e9cd02c
Anuj Attri <anujattri01@gmail.com>
2020-07-17 00:51:52 +0530

    Changes in webcam_utils.py

--- webcam_utils.py
+++ webcam_utils.py
@@ -54,74 +54,74 @@ def realtime_emotions():
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # find face in the frame
-        faces = faceCascade.detectMultiScale(
-                    gray,
-                    scaleFactor=1.1,
-                    minNeighbors=5,
-                    minSize=(30, 30),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
+            faces = faceCascade.detectMultiScale(
+                        gray,
+                        scaleFactor=1.1,
+                        minNeighbors=5,
+                        minSize=(30, 30),
+                        flags=cv2.CASCADE_SCALE_IMAGE
+                    )
         
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            # required region for the face
-            roi_color = frame[y-90:y+h+70, x-50:x+w+50]
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                # required region for the face
+                roi_color = frame[y-90:y+h+70, x-50:x+w+50]
 
-            # save the detected face
-            cv2.imwrite(save_loc, roi_color)
-            # draw a rectangle bounding the face
-            cv2.rectangle(frame, (x-10, y-70),
-                            (x+w+20, y+h+40), (15, 175, 61), 4)
-            
-            # keeps track of waiting time for emotion recognition
-            curr_time = time.time()
-            # do prediction only when the required elapsed time has passed 
-            if curr_time - prev_time >=1:
-                # read the saved image
-                img = cv2.imread(save_loc, 0)
-                
-                if img is not None:
-                    # indicates that prediction has been done atleast once
-                    once = True
+                # save the detected face
+                cv2.imwrite(save_loc, roi_color)
+                # draw a rectangle bounding the face
+                cv2.rectangle(frame, (x-10, y-70),
+                                (x+w+20, y+h+40), (15, 175, 61), 4)
 
-                    # resize image for the model
-                    img = cv2.resize(img, (48, 48))
-                    img = np.reshape(img, (1, 48, 48, 1))
-                    # do prediction
-                    result = model.predict(img)
-                    print(EMOTIONS[np.argmax(result[0])])
-                    
-                #save the time when the last face recognition task was done
-                prev_time = time.time()
+                # keeps track of waiting time for emotion recognition
+                curr_time = time.time()
+                # do prediction only when the required elapsed time has passed
+                if curr_time - prev_time >=1:
+                    # read the saved image
+                    img = cv2.imread(save_loc, 0)
 
-            if once == True:
-                total_sum = np.sum(result[0])
-                # select the emoji face with highest confidence
-                emoji_face = emoji_faces[np.argmax(result[0])]
-                for index, emotion in enumerate(EMOTIONS):
-                    text = str(
-                        round(Decimal(result[0][index]/total_sum*100), 2) ) + "%"
-                    # for drawing progress bar
-                    cv2.rectangle(frame, (100, index * 20 + 10), (100 +int(result[0][index] * 100), (index + 1) * 20 + 4),
-                                    (255, 0, 0), -1)
-                    # for putting emotion labels
-                    cv2.putText(frame, emotion, (10, index * 20 + 20),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (7, 109, 16), 2)
-                    # for putting percentage confidence
-                    cv2.putText(frame, text, (105 + int(result[0][index] * 100), index * 20 + 20),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                    
-                    
-                # overlay emoji on the frame for all the channels
-                for c in range(0, 3):
-                    # for doing overlay we need to assign weights to both foreground and background
-                    foreground = emoji_face[:, :, c] * (emoji_face[:, :, 3] / 255.0)
-                    background = frame[350:470, 10:130, c] * (1.0 - emoji_face[:, :, 3] / 255.0)
-                    frame[350:470, 10:130, c] = foreground + background
-            break
+                    if img is not None:
+                        # indicates that prediction has been done atleast once
+                        once = True
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
+                        # resize image for the model
+                        img = cv2.resize(img, (48, 48))
+                        img = np.reshape(img, (1, 48, 48, 1))
+                        # do prediction
+                        result = model.predict(img)
+                        print(EMOTIONS[np.argmax(result[0])])
+
+                    #save the time when the last face recognition task was done
+                    prev_time = time.time()
+
+                if once == True:
+                    total_sum = np.sum(result[0])
+                    # select the emoji face with highest confidence
+                    emoji_face = emoji_faces[np.argmax(result[0])]
+                    for index, emotion in enumerate(EMOTIONS):
+                        text = str(
+                            round(Decimal(result[0][index]/total_sum*100), 2) ) + "%"
+                        # for drawing progress bar
+                        cv2.rectangle(frame, (100, index * 20 + 10), (100 +int(result[0][index] * 100), (index + 1) * 20 + 4),
+                                        (255, 0, 0), -1)
+                        # for putting emotion labels
+                        cv2.putText(frame, emotion, (10, index * 20 + 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (7, 109, 16), 2)
+                        # for putting percentage confidence
+                        cv2.putText(frame, text, (105 + int(result[0][index] * 100), index * 20 + 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+
+
+                    # overlay emoji on the frame for all the channels
+                    for c in range(0, 3):
+                        # for doing overlay we need to assign weights to both foreground and background
+                        foreground = emoji_face[:, :, c] * (emoji_face[:, :, 3] / 255.0)
+                        background = frame[350:470, 10:130, c] * (1.0 - emoji_face[:, :, 3] / 255.0)
+                        frame[350:470, 10:130, c] = foreground + background
+                break
+
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
