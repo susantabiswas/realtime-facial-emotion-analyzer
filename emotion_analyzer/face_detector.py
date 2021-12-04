77a591e65e3564e344a9b98350f4e185fc3d4d19
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-04 21:38:21 +0530

    face detector base class added

--- emotion_analyzer/face_detector.py
+++ emotion_analyzer/face_detector.py
@@ -4,3 +4,10 @@
 # ===================================================
 """Description: Abstract class for face detectors"""
 # ===================================================
+from abc import ABC, abstractmethod
+
+
+class FaceDetector(ABC):
+    @abstractmethod
+    def detect_faces(self):
+        pass
