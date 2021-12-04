3206a840e60b76362713c2d930a7363609bf5bcb
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-04 20:51:23 +0530

    Deleted old project files

--- visualize_model.py
+++
@@ -1,5 +0,0 @@
-from model_utils import define_model
-from keras.utils.vis_utils import plot_model
-
-model = define_model()
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
