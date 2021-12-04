9d5f58281547da7ad9f5aaf17082ab6feff449ca
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-04 23:45:02 +0530

    mtcnn test cases added

--- tests/test_face_detection_mtcnn.py
+++ tests/test_face_detection_mtcnn.py
@@ -1,8 +1,14 @@
-import pytest
-from emotion_analyzer.exceptions import ModelFileMissing
+from emotion_analyzer.exceptions import InvalidImage
 from emotion_analyzer.face_detection_mtcnn import FaceDetectorMTCNN
-
+import numpy as np
+import pytest   
 
 def test_detect_face(img2_data, img2_facebox_mtcnn):
     ob = FaceDetectorMTCNN()
     assert img2_facebox_mtcnn == ob.detect_faces(img2_data)
+
+def test_invalid_image():
+    ob = FaceDetectorMTCNN()
+    img = np.zeros((100,100,5), dtype='float32')
+    with pytest.raises(InvalidImage):
+        ob.detect_faces(img)
