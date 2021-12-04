56e8c4f76e587e7df631487682c5efba8905584e
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-04 21:55:32 +0530

    Base code for mtcnn face detector

---
+++ emotion_analyzer/face_detection_mtcnn.py
@@ -0,0 +1,14 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+"""Description: Class for face detection. Uses a MTCNN 
+based neural network to get the bounding box coordinates 
+for a human face.
+
+Usage: python -m face_recog.face_detection_mtcnn
+
+You can install mtcnn using PIP by typing "pip install mtcnn"
+Ref: https://github.com/ipazc/mtcnn
+"""
+# ===================================================
