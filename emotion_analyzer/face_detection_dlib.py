b89562997b6ab29aaf291c72247197935d4ca767
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-04 21:54:52 +0530

    dlib face detector base code

---
+++ emotion_analyzer/face_detection_dlib.py
@@ -0,0 +1,12 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+"""Description: Class for face detection. Uses face detectors
+from dlib.
+
+Usage: python -m face_recog.face_detection_dlib
+
+Ref: http://dlib.net/cnn_face_detector.py.html
+"""
+# ===================================================
