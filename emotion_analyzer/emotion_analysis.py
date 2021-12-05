3b7fbef9e244b69057aa2878b3691d960dd7b106
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-05 11:08:37 +0530

    docs added to emotion analyzer file

--- emotion_analyzer/emotion_analysis.py
+++ emotion_analyzer/emotion_analysis.py
@@ -2,3 +2,9 @@
 # ===================================================
 # Author: Susanta Biswas
 # ===================================================
+"""Description: Class for emotion analysis
+
+Usage: python -m emotion_analyzer.emotion_analysis
+
+"""
+# ===================================================
