850f925a82f081de0beb90180e5561c72d0dbd6b
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-05 11:23:37 +0530

    docs added to model utils

---
+++ emotion_analyzer/model_utils.py
@@ -0,0 +1,10 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+"""Description: Helper methods for Emotion model related work
+
+Usage: python -m emotion_analyzer.model_utils
+
+"""
+# ===================================================
