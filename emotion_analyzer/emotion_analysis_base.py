a9c629b3e0a3c0f935ba9a4eb97786e9fc29bc5b
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-05 11:09:11 +0530

    base emotion analysis class added

---
+++ emotion_analyzer/emotion_analysis_base.py
@@ -0,0 +1,10 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+"""Description: Base class for emotion analysis
+
+Usage: python -m emotion_analyzer.emotion_analysis_base
+
+"""
+# ===================================================
