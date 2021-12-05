2761e8b3424f29f14a1766828ebdbd6322a77d17
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-05 11:04:58 +0530

    added docs to dlib tests

--- tests/test_face_detection_dlib.py
+++ tests/test_face_detection_dlib.py
@@ -1,3 +1,9 @@
+# ---- coding: utf-8 ----
+# ===================================================
+# Author: Susanta Biswas
+# ===================================================
+"""Description: Tests for dlib face detector."""
+# ===================================================
 from emotion_analyzer.exceptions import ModelFileMissing, InvalidImage
 from emotion_analyzer.face_detection_dlib import FaceDetectorDlib
 import pytest
