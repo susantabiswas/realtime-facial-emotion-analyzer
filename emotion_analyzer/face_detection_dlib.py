a9ab4b7f0d6df65051988ed83c7ad387c03a9faf
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2021-12-05 10:49:30 +0530

    face detection code added to dlib detector

--- emotion_analyzer/face_detection_dlib.py
+++ emotion_analyzer/face_detection_dlib.py
@@ -72,3 +72,64 @@ class FaceDetectorDlib(FaceDetector):
             logger.info("dlib: {} face detector loaded...".format(self.model_type))
         except Exception as e:
             raise e
+
+
+    def detect_faces(self, image, num_upscaling: int = 1) -> List[List[int]]:
+        """Performs facial detection on an image. Works best with
+        RGB image. Uses a dlib based detector either HOG or MMOD.
+
+        Args:
+            image (numpy array):
+            num_upscaling (int, optional): Number of times to upscale
+            while detecting faces. Defaults to 1.
+
+        Raises:
+            InvalidImage: When the image is either None or
+            with wrong number of channels.
+
+        Returns:
+            List[List[int]]: List of bounding box coordinates
+        """
+        if not is_valid_img(image):
+            raise InvalidImage
+        return [
+            self.dlib_rectangle_to_list(bbox)
+            for bbox in self.face_detector(image, num_upscaling)
+        ]
+
+    def dlib_rectangle_to_list(self, dlib_bbox) -> List[int]:
+        """Converts a dlib rectangle / mmod rectangle to
+        List(top left x, top left y, bottom right x, bottom right y)
+
+        Args:
+            dlib_bbox (dlib.rectangle):
+
+        Returns:
+            List[int]: Bounding box coordinates
+        """
+        # if it is MMOD type rectangle
+        if type(dlib_bbox) == dlib.mmod_rectangle:
+            dlib_bbox = dlib_bbox.rect
+        # Top left corner
+        x1, y1 = dlib_bbox.tl_corner().x, dlib_bbox.tl_corner().y
+        width, height = dlib_bbox.width(), dlib_bbox.height()
+        # Bottom right point
+        x2, y2 = x1 + width, y1 + height
+
+        return [x1, y1, x2, y2]
+
+    def __repr__(self):
+        return "FaceDetectorDlib"
+
+
+if __name__ == "__main__":
+    
+    # Sample Usage
+    # ob = FaceDetectorDlib(model_type="hog")
+    # img = cv2.imread("data/sample/2.jpg")
+    # print(img.shape)
+    # bbox = ob.detect_faces(convert_to_rgb(img))
+    # print(bbox)
+    
+    # draw_bounding_box(img, bbox)
+    pass
